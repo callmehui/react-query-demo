8760295244293d39cb32f8b617e1bd8ea701bc68
zhonghui <1126765590@qq.com>
2021-12-06 23:25:47 +0800

    feat: 添加到第二十三节课的代码

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,10 +1,23 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
-import { useSuperHeroerData } from "../hooks/useSuperHeroesData";
+import {
+  useSuperHeroerData,
+  useAddSuperHeroData,
+} from "../hooks/useSuperHeroesData";
 
 /** SuperHeros页 */
 const RQSuperHeroesPage = () => {
+  const [name, setName] = useState("");
+  const [alterEgo, setAlterEgo] = useState("");
+
   const { isLoading, isFetching, isError, data, error, refetch } =
     useSuperHeroerData();
+
+  const { mutate } = useAddSuperHeroData();
+
+  const handleAddHeroClick = () => {
+    mutate({ name, alterEgo });
+  };
 
   if (isLoading || isFetching) {
     return <h2>Loading...</h2>;
@@ -17,6 +30,11 @@ const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQ Super Heros Page</h2>
+      <div>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+        <input value={alterEgo} onChange={(e) => setAlterEgo(e.target.value)} />
+        <button onClick={handleAddHeroClick}>Add super Hero</button>
+      </div>
       <button onClick={refetch}>refresh</button>
       {data?.data.map((hero) => {
         return (
--- src/hooks/useSuperHeroesData.js
+++ src/hooks/useSuperHeroesData.js
@@ -1,8 +1,12 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 const fetchSuperHeros = () => {
   return axios.get("http://localhost:4000/superheroes");
+};
+
+const addSuperHero = (hero) => {
+  return axios.post("http://localhost:4000/superheroes", hero);
 };
 
 export const useSuperHeroerData = (onSuccess, onError) => {
@@ -13,5 +17,23 @@ export const useSuperHeroerData = (onSuccess, onError) => {
     //   const superHeroNames = data.data.map((hero) => hero.name);
     //   return superHeroNames;
     // },
+  });
+};
+
+export const useAddSuperHeroData = () => {
+  const queryClient = useQueryClient();
+  return useMutation(addSuperHero, {
+    onSuccess: (data) => {
+      /** 使缓存数据失效，并重新请求缓存数据 */
+      // queryClient.invalidateQueries("super-heroes");
+
+      /** 利用新增的返回数据更新缓存，减少一次数据请求 */
+      queryClient.setQueryData("super-heroes", (oldQueryData) => {
+        return {
+          ...oldQueryData,
+          data: [...oldQueryData.data, data.data],
+        };
+      });
+    },
   });
 };
